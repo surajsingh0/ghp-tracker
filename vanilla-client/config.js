85be92c3f4c61558c41a5458dd88368beae6b32f
surajsingh0 <suraj752305@gmail.com>
2024-10-22 19:55:48 +0530

    update baseurl

--- vanilla-client/config.js
+++ vanilla-client/config.js
@@ -1,6 +1,6 @@
 const DEV_BASE_URL = "http://localhost:5000";
 const PROD_BASE_URL =
-    "https://cpuw44k4g0.execute-api.us-east-1.amazonaws.com/production/";
+    "https://cpuw44k4g0.execute-api.us-east-1.amazonaws.com/production";
 const hostname = window.location.hostname;
 
 const baseUrl = ["127.0.0.1", "localhost"].includes(hostname)
