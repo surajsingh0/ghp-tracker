e1069ae3f224aa18a0a60c76ccc57b23a747e1a7
surajsingh0 <suraj752305@gmail.com>
2024-10-22 19:09:06 +0530

    allow migrations for prod only

--- flask-backend-api/manage.py
+++ flask-backend-api/manage.py
@@ -1,8 +1,8 @@
 from flask_migrate import Migrate
 from app import create_app, db
-import os
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+# Migrate in production only
+app = create_app('production')
 migrate = Migrate(app, db)
 
 if __name__ == '__main__':
