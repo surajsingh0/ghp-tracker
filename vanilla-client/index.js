9fc2a5c247b365fa2a1509e728dcc70cba7d233b
surajsingh0 <suraj752305@gmail.com>
2024-10-22 19:49:36 +0530

    fix routing

--- vanilla-client/index.js
+++ vanilla-client/index.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", async () => {
     const isLoggedIn = await verifyUserLogin();
 
     if (!isLoggedIn) {
-        window.location.href = "./landing.html";
+        window.location.href = "landing.html";
     } else {
-        window.location.href = "./ghp-home.html";
+        window.location.href = "ghp-home.html";
     }
 });
